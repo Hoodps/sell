22b00e407b37b8a27ff106a828c405d06aea00a8
Hoodps <791859814@qq.com>
2018-11-23 15:57:24 +0800

    5-4

--- src/main/java/com/hoodps/demo/controller/BuyerProductController.java
+++ src/main/java/com/hoodps/demo/controller/BuyerProductController.java
@@ -3,26 +3,60 @@ package com.hoodps.demo.controller;
 import com.hoodps.demo.VO.ProductInfoVO;
 import com.hoodps.demo.VO.ProductVO;
 import com.hoodps.demo.VO.ResultVO;
+import com.hoodps.demo.dataobject.ProductCategory;
+import com.hoodps.demo.dataobject.ProductInfo;
+import com.hoodps.demo.service.CategoryService;
+import com.hoodps.demo.service.ProductService;
+import com.hoodps.demo.utils.ResultVOUtil;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/buyer/product")
 public class BuyerProductController {
 
+    @Autowired
+    private ProductService productService;
+
+    @Autowired
+    private CategoryService categoryService;
+
     @GetMapping("/list")
     public ResultVO list(){
-        ResultVO resultVO = new ResultVO();
-        ProductVO productVO = new ProductVO();
-        ProductInfoVO productInfoVO = new ProductInfoVO();
 
-        productVO.setProductInfoVOList(Arrays.asList(productInfoVO));
-        resultVO.setData(productVO);
-        resultVO.setCode(0);
-        resultVO.setMsg("成功");
-        return resultVO;
+        List<ProductInfo> productInfoList = productService.findUpAll();
+
+        List<Integer> categoryTypeList = productInfoList.stream()
+                .map(e -> e.getCategoryType())
+                .collect(Collectors.toList());
+        List<ProductCategory> productCategoryList = categoryService.findByCategoryTypeIn(categoryTypeList);
+
+        List<ProductVO> productVOList = new ArrayList<>();
+        for (ProductCategory productCategory: productCategoryList) {
+            ProductVO productVO = new ProductVO();
+            productVO.setCategoryType(productCategory.getCategoryType());
+            productVO.setCategoryName(productCategory.getCategoryName());
+
+            List<ProductInfoVO> productInfoVOList = new ArrayList<>();
+            for (ProductInfo productInfo: productInfoList) {
+                if (productInfo.getCategoryType().equals(productCategory.getCategoryType())) {
+                    ProductInfoVO productInfoVO = new ProductInfoVO();
+                    BeanUtils.copyProperties(productInfo, productInfoVO);
+                    productInfoVOList.add(productInfoVO);
+                }
+            }
+            productVO.setProductInfoVOList(productInfoVOList);
+            productVOList.add(productVO);
+        }
+
+        return ResultVOUtil.success(productVOList);
     }
 }
